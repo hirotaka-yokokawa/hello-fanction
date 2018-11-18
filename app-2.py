abb5d5d51e528acb04aea70e2a573e1d8b3be6d7
Hirotaka <yokokawa.491992@gmail.com>
2018-11-18 10:44:18 +0900

    リファクタリング_コントロール-> t マジ便利

---
+++ app-2.py
@@ -0,0 +1,4 @@
+# 割り勘したーい 7000
+from warikan2 import calculate_warikan
+
+print(calculate_warikan(amount=7000, number_of_people=6))
---
+++ warikan2.py
@@ -0,0 +1,2 @@
+def calculate_warikan(amount, number_of_people):
+    return f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円"
