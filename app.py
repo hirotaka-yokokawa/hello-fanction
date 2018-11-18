c70c6a2644ad835944c88f13377c5bec8198bd55
Hirotaka <yokokawa.491992@gmail.com>
2018-11-18 09:22:46 +0900

    dekitannkobu

---
+++ app.py
@@ -0,0 +1,34 @@
+def hello():
+    print("hello world")
+
+
+hello()
+
+
+def say_hello(name):
+    print(f"Hi {name}")
+
+
+say_hello("Bob")
+
+
+def double(number):
+    return 2 * number
+
+
+result_1 = double(3)
+
+print(result_1)
+
+name = "Bob"
+print(name)
+
+
+# 1分課題
+def str_combine(str1, str2, ):
+    return str1 + str2
+
+
+result = str_combine("Kazuma", "Takahasi")
+
+print(result)
